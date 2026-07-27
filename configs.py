6d5de3d27965cb360615c4c2d606d9d67b9b775d
kishorekumar12356z-maker <kishorekumar12356z@gmail.com>
2026-07-27 14:26:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,14 +12,15 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "24681559"))
+    API_HASH = os.environ.get("API_HASH", "d967929e0100f3c119bb356e43d6fa77")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8653566072:AAG-MvuHXxw17MEQj_lP7HufkCKyzVvPayI")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 8030254824))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "
+mongodb+srv://vegitobule77_db_user:gokuda@cluster0.hgifx7y.mongodb.net/?appName=Cluster0")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003985227353"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
